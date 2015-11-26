358da464ae820a800ccfb430bbdb1cb92bb103f1
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-11-26 15:09:41 +0200

    Implementing radio input

---
+++ src/MenuPages/Fields/Radio.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Radio.php description
+ *
+ * @author    Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * @date      2015-11-21
+ * @since     TODO ${VERSION}
+ * @package   Pan\MenuPages\Fields
+ * @copyright Copyright (c) 2015 Panagiotis Vagenas
+ */
+
+namespace Pan\MenuPages\Fields;
+
+/**
+ * Class Radio
+ *
+ * @author    Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * @date      2015-11-21
+ * @since     TODO ${VERSION}
+ * @package   Pan\MenuPages\Fields
+ * @copyright Copyright (c) 2015 Panagiotis Vagenas
+ */
+class Radio extends Checkbox{
+    protected $type = 'radio';
+}
