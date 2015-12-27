ee1962c262fe693cb865430f14f6ba3871ac9cb2
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-12-27 10:38:44 +0200

    Removed some unused constants

--- src/MenuPages/Scripts/Ifc/IfcScripts.php
+++ src/MenuPages/Scripts/Ifc/IfcScripts.php
@@ -14,33 +14,51 @@ use Pan\MenuPages\Ifc\IfcConstants;
  * @copyright Copyright (c) ${YEAR} Panagiotis Vagenas.
  */
 interface IfcScripts extends IfcConstants{
+    /**
+     *
+     */
     const ASSETS_FOLDER = 'assets';
+    /**
+     *
+     */
     const CORE_JS_SLUG = 'wp-menu-pages-js';
+    /**
+     *
+     */
     const REQUIRE_JS_SLUG = 'require-js';
+    /**
+     *
+     */
     const CORE_CSS_SLUG = 'wp-menu-pages-css';
+    /**
+     *
+     */
     const CORE_JS_DEFINITIONS = 'wpMenuPagesDefinitions';
+    /**
+     *
+     */
     const CORE_JS_OBJECT = 'wpMenuPages';
 
+    /**
+     *
+     */
     const ACTION_SAVE_PREFIX = 'wp-menu-pages-save-options-';
+    /**
+     *
+     */
     const ACTION_RESET_PREFIX = 'wp-menu-pages-reset-options-';
+    /**
+     *
+     */
     const ACTION_EXPORT_PREFIX = 'wp-menu-pages-export-options-';
+    /**
+     *
+     */
     const ACTION_IMPORT_PREFIX = 'wp-menu-pages-import-options-';
+    /**
+     *
+     */
     const ACTION_UPDATE_CORE_OPTIONS_PREFIX = 'wp-menu-pages-update-core-options-';
-
-    /**
-     * Bootstrap CSS Slug
-     */
-    const SLUG_BOOTSTRAP_CSS = 'wp-menu-pages-bs-css';
-
-    /**
-     * Bootstrap CSS Slug
-     */
-    const SLUG_BOOTSTRAP_THEME_CSS = 'wp-menu-pages-bs-theme-css';
-
-    /**
-     * Bootstrap JS Slug
-     */
-    const SLUG_BOOTSTRAP_JS = 'wp-menu-pages-bs-js';
 
     /**
      * Select2 CSS Slug
@@ -48,29 +66,9 @@ interface IfcScripts extends IfcConstants{
     const SLUG_SELECT2_CSS = 'wp-menu-pages-select2-css';
 
     /**
-     * Select2 JS Slug
-     */
-    const SLUG_SELECT2_JS = 'wp-menu-pages-select2-js';
-
-    /**
      * FontAwesome CSS Slug
      */
     const SLUG_FONT_AWESOME_CSS = 'wp-menu-pages-font-awesome-css';
-
-    /**
-     * Bootstrap JS CDN
-     */
-    const CDN_BOOTSTRAP_JS = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js';
-
-    /**
-     * Select2 CSS CDN
-     */
-    const CDN_SELECT2_CSS = 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css';
-
-    /**
-     * Select2 JS CDN
-     */
-    const CDN_SELECT2_JS = 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.full.min.js';
 
     /**
      * FontAwesome CSS CDN
