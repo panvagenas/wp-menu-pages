3c7623dc389eaf4aa88c240767b8cb270889c035
Panagiotis Vagenas <pan.vagenas@gmail.com>
2016-01-14 11:53:32 +0200

    Supporting submenu page for wp built in pages

--- src/MenuPages/Pages/SubPage.php
+++ src/MenuPages/Pages/SubPage.php
@@ -23,6 +23,19 @@ use Pan\MenuPages\WpMenuPages;
  * @copyright Copyright (c) 2015 Panagiotis Vagenas
  */
 class SubPage extends Page {
+    const PARENT_DASHBOARD = 'index.php';
+    const PARENT_POSTS = 'edit.php';
+    const PARENT_MEDIA = 'upload.php';
+    const PARENT_LINKS = 'link-manager.php';
+    const PARENT_PAGES = 'edit.php?post_type=page';
+    const PARENT_COMMENTS = 'edit-comments.php';
+    const PARENT_APPEARANCE = 'themes.php';
+    const PARENT_PLUGINS = 'plugins.php';
+    const PARENT_USERS = 'users.php';
+    const PARENT_TOOLS = 'tools.php';
+    const PARENT_SETTINGS = 'options-general.php';
+    const PARENT_SETTINGS_NET = 'settings.php';
+
     /**
      * @var Page
      */
@@ -30,7 +43,7 @@ class SubPage extends Page {
 
     public function __construct(
         WpMenuPages $menuPages,
-        Page $parent,
+        $parent,
         $menuTitle,
         $menuSlug = '',
         $title = '',
@@ -40,13 +53,18 @@ class SubPage extends Page {
         $position = null
     ) {
         parent::__construct( $menuPages, $menuTitle, $menuSlug, $title, $capability, $subtitle, $iconUrl, $position );
+
+        if(!(is_string($parent) || $parent instanceof Page)){
+            throw new \InvalidArgumentException('Wrong parent page');
+        }
+
         $this->parent = $parent;
     }
 
 
     public function init() {
         $this->hookSuffix = add_submenu_page(
-            $this->parent->getMenuSlug(),
+            is_string($this->parent) ? $this->parent : $this->parent->getMenuSlug(),
             $this->title,
             $this->menuTitle,
             $this->capability,
