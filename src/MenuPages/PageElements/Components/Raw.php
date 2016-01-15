fdc1b370139970d3195c380582747dfd60f681eb
Panagiotis Vagenas <pan.vagenas@gmail.com>
2016-01-15 13:54:48 +0200

    Raw component

---
+++ src/MenuPages/PageElements/Components/Raw.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pan\MenuPages\PageElements\Components;
+
+use Pan\MenuPages\PageElements\Components\Abs\AbsComponent;
+
+class Raw extends AbsComponent{
+    protected $content = '';
+    protected $templateName = 'raw.twig';
+
+    /**
+     * @return string
+     * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+     * @see    Raw::$content
+     * @codeCoverageIgnore
+     */
+    public function getContent() {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return $this
+     * @author Panagiotis Vagenas <pan.vagenas@gmail.com>
+     * @codeCoverageIgnore
+     */
+    public function setContent( $content ) {
+        $this->content = $content;
+
+        return $this;
+    }
+}
