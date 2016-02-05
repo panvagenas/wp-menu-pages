cc92baf01997991c1ecbfc762dff898176c20837
Panagiotis Vagenas <pan.vagenas@gmail.com>
2016-02-05 11:33:58 +0200

    Removed some unused constants

--- src/MenuPages/Templates/Ifc/IfcTemplateConstants.php
+++ src/MenuPages/Templates/Ifc/IfcTemplateConstants.php
@@ -5,10 +5,6 @@ namespace Pan\MenuPages\Templates\Ifc;
 use Pan\MenuPages\Ifc\IfcConstants;
 
 interface IfcTemplateConstants extends IfcConstants {
-    const FILTER_PATHS_PREFIX = 'wp_menu_pages_filter_template_paths_';
-
-    const FILTER_PATHS = 'wp_menu_pages_filter_template_paths';
-
     const NO_TEMPLATE_PATH = 'misc/no-template.twig';
 
     const TEMPLATES_DIR = 'templates';
